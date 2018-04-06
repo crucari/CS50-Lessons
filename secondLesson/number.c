83774cd3619f2643925dbe28d8fa13ae93d46d41
Carina Cruz <lecruz@bu.edu>
2018-04-06 03:23:23 +0000

    secondLesson

--- secondLesson/number.c
+++ secondLesson/number.c
@@ -6,9 +6,16 @@ int stuff()
      return 1;  //sends this to the other line of code
 }
 
-int main()
+int main(void)
 {
-    stuff("Hello");
-    printf("Hello there\n");
 
+    printf("Hello there\n");
+    int integer = 5 + 8;
+    // 001101 = 000101 + 000101
+    printf("This is my number: %i\n", integer);
+    //strings don't exist. Cancating does not exist in C
+    float flt = 5.3 + 4.83;
+    printf("This is my float: %.20f\n", flt);
 }
+
+//how to create a variable to store an interger
