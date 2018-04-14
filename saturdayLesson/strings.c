432d354d5631809653745e94ffd3947be60121dc
Carina Cruz <lecruz@bu.edu>
2018-04-14 15:41:15 +0000

    strings updated

--- saturdayLesson/strings.c
+++ saturdayLesson/strings.c
@@ -4,9 +4,23 @@
 
 int main(int argc, char *argv[])
     {
-        printf("%i\n", argc);
-        int length = strlen(argv[1]);
-        printf("%i\t%s\n", length, argv[0]);
+        if (argc !=2) //if argc is not equal to 2
+             {
+                printf("Usage: strings \"phase\"\n");
+                exit(1);  //return 0 or 1 is another way
+             }
+        // printf("%i\n", argc);
+        // int length = strlen(argv[1]);
+        // printf("%i\n", length);
+
+        for(int i = 0, length = strlen(argv[1]); i < length; i++)
+
+        {
+             if (argv[1][i] != ' ')
+            {
+             printf("the ascii value at pos %i: %i\n", i, argv[1][i]);
+            }
+        }
     }
 
 
